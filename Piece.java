1b21307db3a24c031157d0b7ff575ac5b53f78a3
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-07-31 16:59:07 -0700

    Update Piece.java
    This is the file for all the offsets of pieces i.e how to move the pieces and tell if they are invalid moves

--- Piece.java
+++ Piece.java
@@ -1,11 +1,71 @@
-public class Piece {
+import java.util.*;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+public class Piece{
 	
+	public ArrayList<Integer> Bishop_Offsets(String x, int u) {
+		ArrayList<Integer> _Offsets = new ArrayList<Integer>();
+		int t = u;
+		if (x == "ul") {
+			while (t > 8) {
+				t -= 9;
+				_Offsets.add(t);
+			}
+		} else if (x == "ur") {
 
-	Piece(String name, String type) {
+			while (t > 7) {
+				t-= 7;
+				_Offsets.add(t);
+			}
 
-		this.name = name;
+		} else if (x == "dr") {
 
-		this.type = type;
+			while (t < 56) {
+				t += 9;
+				_Offsets.add(t);
+			}
+		} else if (x == "dl") {
+			while(t < 58) {
+				t += 7;
+				_Offsets.add(t);
+			}
+		}
+
+		return _Offsets;
+
+	}
+
+	public ArrayList<Integer> Knight_Offsets(int u) { //u is the starting position
+
+		ArrayList<Integer> _Offsets_Knight = new ArrayList<Integer>();
+
+		if (u - 8 >= 1) {
+			_Offsets_Knight.add(u-6);
+			_Offsets_Knight.add(u-10);
+		}
+		if (u - 16 >= 1) {
+			_Offsets_Knight.add(u-15);
+			_Offsets_Knight.add(u-17);
+		}
+		if (u + 8 <= 64) {
+			_Offsets_Knight.add(u + 6);
+			_Offsets_Knight.add(u +10);
+		}
+		if (u + 16 <= 64) {
+			_Offsets_Knight.add(u + 15);
+			_Offsets_Knight.add(u + 17);
+		}
+
+		return _Offsets_Knight; 
+
+	}
+
+	Piece() {
+
+
 
 	}
 }
